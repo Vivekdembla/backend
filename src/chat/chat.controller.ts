d2d9d0d65140d1c8e7ca830fd0d8164a3d42b163
Amandeep <amandeep@Amandeeps-MacBook-Air.local>
2025-01-07 08:38:07 +0530

    udates

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, Res } from '@nestjs/common';
 import { OpenAIProvider } from './openai.provider';
 import { Request, Response } from 'express';
 import { BASE_PROMPT, getSystemPrompt } from 'src/prompts';
@@ -8,58 +8,63 @@ import { basePrompt as nodeBasePrompt } from 'src/default/node';
 import { ChatCompletionMessageParam } from 'openai/resources';
 import { MessagesDTO } from './messages.dto';
 
-
-
 @Controller()
 export class ChatController {
-    constructor(private readonly openAIProvider: OpenAIProvider) { }
-    @Get('/initial-prompts')
-    async generatePrompt(@Res() res: Response, @Req() req: Request) {
-        const prompt = req.query.prompt.toString();
-        console.log(prompt, 'prompt')
-        const openai = this.openAIProvider.getInstance();
-        try {
-            // const response = await openai.chat.completions.create({
-            //     model: "gpt-4o-mini",
-            //     messages: [{ role: "user", content: prompt }, { role: "system", content: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra" }],
-            // });
-            // const answer = response.choices[0].message.content
-            const answer = "react";
-            if (answer == "react") {
-                res.json({
-                    prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
-                    uiPrompts: [reactBasePrompt]
-                })
-                return;
-            }
+  constructor(private readonly openAIProvider: OpenAIProvider) {}
+  @Get('/initial-prompts')
+  async generatePrompt(@Res() res: Response, @Req() req: Request) {
+    const prompt = req.query.prompt.toString();
+    console.log(prompt, 'prompt');
+    const openai = this.openAIProvider.getInstance();
+    try {
+      // const response = await openai.chat.completions.create({
+      //     model: "gpt-4o-mini",
+      //     messages: [{ role: "user", content: prompt }, { role: "system", content: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra" }],
+      // });
+      // const answer = response.choices[0].message.content
+      const answer = 'react';
+      if (answer == 'react') {
+        res.json({
+          prompts: [
+            BASE_PROMPT,
+            `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+          ],
+          uiPrompts: [reactBasePrompt],
+        });
+        return;
+      }
 
-            if (answer === "node") {
-                res.json({
-                    prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
-                    uiPrompts: [nodeBasePrompt]
-                })
-                return;
-            }
-            res.status(403).json({ message: "You cant access this" })
-
-        } catch (error) {
-            console.error(error);
-            res.status(400).send(`Error fetching data`);
-        }
-
+      if (answer === 'node') {
+        res.json({
+          prompts: [
+            `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+          ],
+          uiPrompts: [nodeBasePrompt],
+        });
+        return;
+      }
+      res.status(403).json({ message: 'You cant access this' });
+    } catch (error) {
+      console.error(error);
+      res.status(400).send(`Error fetching data`);
     }
+  }
 
-    @Get('/chat')
-    async getPage(@Res() res: Response, @Req() req: Request, @Query('messages') messages: MessagesDTO[]) {
-        // const openai = this.openAIProvider.getInstance();
-        const data: ChatCompletionMessageParam[] = messages.map((content) => {
-            return { role: content.role, content: content.message }
-        })
-        res.json(data);
-        // const response = await openai.chat.completions.create({
-        //     model: "gpt-4o-mini",
-        //     messages: [...data, { role: "system", content: getSystemPrompt() }],
-        // });
-        // res.json(response.choices[0].message.content);
-    }
+  @Post('/chat')
+  async getPage(
+    @Res() res: Response,
+    @Req() req: Request,
+    @Body('messages') messages: MessagesDTO[],
+  ) {
+    const openai = this.openAIProvider.getInstance();
+    const data: ChatCompletionMessageParam[] = messages.map((content) => {
+      return { role: content.role, content: content.message };
+    });
+    console.log(data);
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [...data, { role: 'system', content: getSystemPrompt() }],
+    });
+    res.json(response.choices[0].message.content);
+  }
 }
